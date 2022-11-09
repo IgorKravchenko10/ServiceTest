a9f89daf0f6dbaff1b8e8dfbb726bf0e1cbb61e7
Igor Kravchenko <igorkr.10@gmail.com>
2022-11-09 16:44:29 +0200

    Add project files.

---
+++ ServiceTest/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace ServiceTest;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnCounterClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            string address = "https://tcu.base2base.com.ua:9005/api";
+            BasicHttpConnection connection = new BasicHttpConnection(address);
+            var result = await connection.Post<string[]>("Values", "string", new CancellationToken());
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Exception", ex.Message, "OK");
+        }
+    }
+}
